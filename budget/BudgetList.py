8137c0b70f29660648302b344784db570f989e53
spoynter362 <seth.poynter@getbuilt.com>
2020-12-23 09:42:12 -0600

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -31,7 +31,5 @@ class BudgetList:
 
         print('The count of all expenses: ' + str(len(myBudgetList))
 
-    if __name__ == "__main__":
+    if __name__ == "__main__" :
         main()
-    else:
-        pass
